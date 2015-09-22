92bf573b62d54b3361e9a258845385a99d3e074e
takushi <takushi@mfmf.me>
2015-09-22 17:52:29 +0900

    Initial Commit

---
+++ SweetHomeTheater/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SweetHomeTheater
+//
+//  Created by Homma Takushi on 2015/09/22.
+//  Copyright © 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
